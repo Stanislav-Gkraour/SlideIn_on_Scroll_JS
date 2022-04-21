aa1d5e0b22bee355e0368d9fc2b92749b03de51a
StanTheMan7 <sts.graur@gmail.com>
2022-04-21 21:03:55 +0200

    Slide  in on Scroll

---
+++ main.js
@@ -0,0 +1,41 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+
+// Select all the images that we going to listen to 
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+
+// create a function that runs every time the user scrolls
+
+function checkSlide(e){
+    sliderImages.forEach(sliderImage => {
+        // half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        console.log(slideInAt)
+        // bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if(isHalfShown && isNotScrolledPast){
+            sliderImage.classList.add('active');
+        }else{
+            sliderImage.classList.remove('active')
+        }
+    });
+}
+window.addEventListener('scroll', debounce(checkSlide))
